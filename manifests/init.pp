23adf3416f2ea39c9e3aac792fc071609a18d9f0
Romain Tartière <romain@blogreen.org>
2020-07-20 08:53:00 -1000

    Fix puppet-lint errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,17 @@
+# Common settings for configuring PuppetVPN
+#
+# @param tls_auth_enabled
+# @param tls_auth_content
+# @param admin_group
+# @param admin_user
+# @param config_name
+# @param etcdir
 class puppetvpn (
-  $tls_auth_enabled = false,
-  $tls_auth_content = undef,
-  $admin_group = $puppetvpn::params::admin_group,
-  $admin_user = $puppetvpn::params::admin_user,
-  $config_name = $puppetvpn::params::config_name,
-  $etcdir = $puppetvpn::params::etcdir,
+  Boolean              $tls_auth_enabled = false,
+  Optional[String[1]]  $tls_auth_content = undef,
+  String[1]            $admin_group = $puppetvpn::params::admin_group,
+  String[1]            $admin_user = $puppetvpn::params::admin_user,
+  String[1]            $config_name = $puppetvpn::params::config_name,
+  Stdlib::Absolutepath $etcdir = $puppetvpn::params::etcdir,
 ) inherits puppetvpn::params {
 }
