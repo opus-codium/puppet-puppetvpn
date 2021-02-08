f96930b7a241dcf007a80d5310d25d34fca799af
Romain Tartière <romain@blogreen.org>
2021-02-08 09:37:38 -1000

    Fix CI

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -9,7 +9,8 @@ describe 'puppetvpn' do
       osfamily: 'Debian',
     }
   end
+
   context 'with defaults for all parameters' do
-    it { should contain_class('puppetvpn') }
+    it { is_expected.to contain_class('puppetvpn') }
   end
 end
